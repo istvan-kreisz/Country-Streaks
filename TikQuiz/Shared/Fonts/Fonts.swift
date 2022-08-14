1eae1e873d4df45eb8c29cc7275c3bdff9be8503
istvan-kreisz <kreiszdev@gmail.com>
2022-08-14 12:59:51 +0200

    font update

--- TikQuiz/Shared/Fonts/Fonts.swift
+++ TikQuiz/Shared/Fonts/Fonts.swift
@@ -9,21 +9,20 @@ import UIKit
 import SwiftUI
 
 extension Font {
-    static func light(size: CGFloat) -> Font { return Font.custom("Rubik-Light", size: size) }
-    static func regular(size: CGFloat) -> Font { return Font.custom("Rubik-Regular", size: size) }
-    static func medium(size: CGFloat) -> Font { return Font.custom("Rubik-Medium", size: size) }
-    static func semiBold(size: CGFloat) -> Font { return Font.custom("Rubik-SemiBold", size: size) }
-    static func bold(size: CGFloat) -> Font { return Font.custom("Rubik-Bold", size: size) }
-    static func extraBold(size: CGFloat) -> Font { return Font.custom("Rubik-ExtraBold", size: size) }
-    static func black(size: CGFloat) -> Font { return Font.custom("Rubik-Black", size: size) }
+    static func light(size: CGFloat) -> Font { return Font.custom("Dosis-Light", size: size) }
+    static func regular(size: CGFloat) -> Font { return Font.custom("Dosis-Regular", size: size) }
+    static func medium(size: CGFloat) -> Font { return Font.custom("Dosis-Medium", size: size) }
+    static func semiBold(size: CGFloat) -> Font { return Font.custom("Dosis-SemiBold", size: size) }
+    static func bold(size: CGFloat) -> Font { return Font.custom("Dosis-Bold", size: size) }
+    static func extraBold(size: CGFloat) -> Font { return Font.custom("Dosis-ExtraBold", size: size) }
 }
 
-extension UIFont {
-    static var light: UIFont { return UIFont(name: "Rubik-Light", size: 12)! }
-    static var regular: UIFont { return UIFont(name: "Rubik-Regular", size: 12)! }
-    static var medium: UIFont { return UIFont(name: "Rubik-Medium", size: 12)! }
-    static var semiBold: UIFont { return UIFont(name: "Rubik-SemiBold", size: 12)! }
-    static var bold: UIFont { return UIFont(name: "Rubik-Bold", size: 12)! }
-    static var extraBold: UIFont { return UIFont(name: "Rubik-ExtraBold", size: 12)! }
-    static var black: UIFont { return UIFont(name: "Rubik-Black", size: 12)! }
-}
+//extension UIFont {
+//    static var light: UIFont { return UIFont(name: "Rubik-Light", size: 12)! }
+//    static var regular: UIFont { return UIFont(name: "Rubik-Regular", size: 12)! }
+//    static var medium: UIFont { return UIFont(name: "Rubik-Medium", size: 12)! }
+//    static var semiBold: UIFont { return UIFont(name: "Rubik-SemiBold", size: 12)! }
+//    static var bold: UIFont { return UIFont(name: "Rubik-Bold", size: 12)! }
+//    static var extraBold: UIFont { return UIFont(name: "Rubik-ExtraBold", size: 12)! }
+//    static var black: UIFont { return UIFont(name: "Rubik-Black", size: 12)! }
+//}
