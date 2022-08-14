ba1fe6944fb63def4a1eb43c42194d9092d61e1f
istvan-kreisz <kreiszdev@gmail.com>
2022-08-14 21:03:16 +0200

    added game overr modal

--- TikQuiz/Views/Play/PlayView.swift
+++ TikQuiz/Views/Play/PlayView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct PlayView: View {
+    private let medalSize: CGFloat = .init(adaptiveSize: 71)
+
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var store: Store
 
@@ -15,14 +17,11 @@ struct PlayView: View {
 
     @State var level: Level
     @State var answerIndex: Int = -1
+    @State var finalResult: Int?
 
     @State var correctAnswer: String
 
-    @State var showAlert = false
-
-    var imageSize: CGFloat {
-        UIScreen.main.bounds.width < 414 ? 400 : 600
-    }
+    @State var isShowingGameOverModal = true
 
     init(level: Level, didBuyRemoveAds: Bool) {
         self._correctAnswer = State<String>(initialValue: level.country.name)
@@ -60,41 +59,97 @@ struct PlayView: View {
     }
 
     var body: some View {
-        VStack(spacing: 10) {
+        ZStack {
+            VStack(spacing: 10) {
+                ZStack {
+                    NavigationBar(title: "", isBackButtonVisible: true)
+                        .layoutPriority(2)
+                    VStack {
+                        Text("Current Streak")
+                            .font(.bold(size: .init(adaptiveSize: 18)))
+                            .foregroundColor(Color.white)
+                        Text("\(store.state.currentStreak)")
+                            .font(.bold(size: .init(adaptiveSize: 20)))
+                            .foregroundColor(Color.white)
+                    }
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 5)
+                    .background(Color.customOrange.opacity(0.6))
+                    .cornerRadius(10)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 2))
+                    .topAligned()
+                    .rightAligned()
+                }
+
+                Spacer()
+
+                VStack(spacing: .init(adaptiveSize: 10)) {
+                    HStack(spacing: .init(adaptiveSize: 25)) {
+                        countryButton(answer: level.answers[0])
+                        countryButton(answer: level.answers[1])
+                    }
+                    HStack(spacing: .init(adaptiveSize: 25)) {
+                        countryButton(answer: level.answers[2])
+                        countryButton(answer: level.answers[3])
+                    }
+                }
+            }
             ZStack {
-                NavigationBar(title: "", isBackButtonVisible: true)
-                    .layoutPriority(2)
-                VStack {
-                    Text("Current Streak")
-                        .font(.bold(size: .init(adaptiveSize: 18)))
-                        .foregroundColor(Color.white)
-                    Text("\(store.state.currentStreak)")
-                        .font(.bold(size: .init(adaptiveSize: 20)))
-                        .foregroundColor(Color.white)
-                }
-                .padding(.horizontal, 12)
-                .padding(.vertical, 5)
-                .background(Color.customOrange.opacity(0.6))
-                .cornerRadius(10)
-                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 2))
-                .topAligned()
-                .rightAligned()
-            }
+                VStack(alignment: .center, spacing: .init(adaptiveSize: 25)) {
+                    VStack(alignment: .center, spacing: .init(adaptiveSize: 20)) {
+                        Text("Streak Over".uppercased())
+                            .font(.system(size: .init(adaptiveSize: 40), weight: .semibold, design: .default))
+                            .italic()
+                            .foregroundColor(.customYellow)
+                        HStack(alignment: .top, spacing: 55) {
+                            VStack {
+                                VStack(alignment: .trailing, spacing: 3) {
+                                    Text("Score")
+                                        .font(.bold(size: .init(adaptiveSize: 24)))
+                                        .foregroundColor(Color.white)
+                                    Text("\(finalResult ?? 0)")
+                                        .font(.bold(size: .init(adaptiveSize: 24)))
+                                        .foregroundColor(Color.white)
+                                }
+                                VStack(alignment: .trailing, spacing: 3) {
+                                    Text("Best")
+                                        .font(.bold(size: .init(adaptiveSize: 24)))
+                                        .foregroundColor(Color.white)
+                                    Text("\(store.state.bestStreak)")
+                                        .font(.bold(size: .init(adaptiveSize: 24)))
+                                        .foregroundColor(Color.white)
+                                }
+                            }
+                            VStack {
+                                Text("Medal")
+                                    .font(.bold(size: .init(adaptiveSize: 24)))
+                                    .foregroundColor(Color.white)
 
-            Spacer()
+                                if let medal = Medal(bestStreakCount: finalResult ?? 0) {
+                                    Image(medal.imageName)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: medalSize)
+                                } else {
+                                    Circle().stroke(Color.white, lineWidth: 2)
+                                        .frame(width: medalSize + 4, height: medalSize + 4)
+                                }
+                            }
+                        }
+                    }
+                    .padding(.vertical, .init(adaptiveSize: 20))
+                    .frame(width: .init(adaptiveSize: 500))
+                    .background(Color.customBlue.opacity(0.9))
+                    .cornerRadius(30)
 
-            VStack(spacing: .init(adaptiveSize: 10)) {
-                HStack(spacing: .init(adaptiveSize: 25)) {
-                    countryButton(answer: level.answers[0])
-                    countryButton(answer: level.answers[1])
-                }
-                HStack(spacing: .init(adaptiveSize: 25)) {
-                    countryButton(answer: level.answers[2])
-                    countryButton(answer: level.answers[3])
+                    HStack(spacing: 20) {
+                        MainButton(text: "OK", fontSize: .init(adaptiveSize: 20), fillColor: .customOrange, width: 110) {}
+                        MainButton(text: "SHARE", fontSize: .init(adaptiveSize: 20), fillColor: .customOrange, width: 110) {}
+                    }
                 }
             }
+            .opacity(finalResult != nil ? 1 : 0)
         }
-        .layoutPriority(1)
         .background(Image(level.attachment)
             .resizable()
             .scaledToFill()
